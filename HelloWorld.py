75f841adea2f4409bba92b6084bbfbac43e98bb5
Ak627 <91678317+Ak627@users.noreply.github.com>
2023-02-07 09:47:36 -0700

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import string
+cases = int(input())
+l = []
+
+for i in range(cases):
+    sentance = input()
+    l.append(sentance)
+
+for i in range(len(l)):
+    print(l[i])
+    
